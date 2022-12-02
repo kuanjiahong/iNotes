80cc47ad0822ec43abd02dca6312b2b5ffe4c0ba
Jia Hong <kuanjiahong@gmail.com>
2022-12-02 16:48:05 +0800

    added login form

--- noteapp/src/App.js
+++ noteapp/src/App.js
@@ -1,26 +1,82 @@
 import logo from './logo.svg';
 import './App.css';
 import $ from 'jquery';
+import React from 'react';
+
+
+class LoginForm extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      name: "",
+      password: "",
+    }
+    this.handleInputChange = this.handleInputChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  handleInputChange(event) {
+    const target = event.target
+    const value = target.value
+    const name = target.name
+    this.setState({
+      [name]: value
+    })
+  }
+
+
+  handleSubmit(event) {
+    event.preventDefault();
+    
+    // send request
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>iNotes</h1>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Name:
+          </label>
+            <input type="text" name="name" value={this.state.name} onChange={this.handleInputChange} />
+          <label>
+            Password:
+          </label>
+            <input type="password" name="password" value={this.state.password} onChange={this.handleInputChange} />
+          <input type="submit" value="Sign in"/>
+        </form>
+      </div>
+    )
+  }
+}
+
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <LoginForm />
+  )
 }
 
 export default App;
